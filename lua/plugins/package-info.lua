c76501b269389799b9e050b56b508ce2b694a2b0
Roman Horokhov <7347253+NerveClasp@users.noreply.github.com>
2025-05-26 13:25:18 +0300

    feat: disable packagle info

--- lua/plugins/package-info.lua
+++ lua/plugins/package-info.lua
@@ -1,3 +1,7 @@
+if true then return {} end
+-- This plugin provides a way to manage package.json dependencies in Neovim.
+-- It's kinda unstable, I'll keep it here for now.
+
 return {
   "vuki656/package-info.nvim",
   config = function()
